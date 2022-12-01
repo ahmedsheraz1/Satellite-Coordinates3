a5ce388373a82c608f4cddcae5a3444c5886b20c
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 08:58:52 +0500

    Create Satellite Coordinates3.py

---
+++ Satellite Coordinates3.py
@@ -0,0 +1,10 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+'{:<30}'.format('left aligned')
+'left aligned                  '
+'{:>30}'.format('right aligned')
+'                 right aligned'
+'{:^30}'.format('centered')
+'           centered           '
+'{:*^30}'.format('centered')
+'***********centered***********'
